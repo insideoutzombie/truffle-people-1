044b6c7d1ff0d0b4d6e5750ae6d068f1759587bc
Aaron Anderson <aaronlynanderson@gmail.com>
2017-12-27 22:12:47 -0600

    Home renders with form component

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import Form from './form.js';
+
+
+let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"))
+let ABI = require('../../../abi/PeopleABI.js');
+let peopleAddress = '0x345ca3e014aaf5dca488057592ee47305d9b3e10';
+let People = web3.eth.contract(ABI).at(peopleAddress);
+
 
 class Home extends Component {
   render() {
     return(
       <main className="container">
-        
+        <Form />
+
       </main>
     )
   }
---
+++ src/layouts/home/form.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
+
+let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"))
+let ABI = require('../../../abi/PeopleABI.js');
+let peopleAddress = '0x345ca3e014aaf5dca488057592ee47305d9b3e10';
+let People = web3.eth.contract(ABI).at(peopleAddress);
+// let addPersonInstance = '';
+web3.eth.defaultAccount = web3.eth.accounts[0];
+// defaultAccount = web3.eth.defaultAccount;
+
+class Form extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      firstName: '',
+      lastName: '',
+      age: '',
+    }
+    this.handleTextChange=this.handleTextChange.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this);
+  }
+
+  handleTextChange = (event) => {
+    event.preventDefault();
+    if (this.state[event.target.id] !== undefined){
+      this.setState({[event.target.id]: event.target.value});
+    }
+  }
+  handleSubmit = (event) => {
+    var payload1 = web3.fromAscii(this.state.firstName,32);
+    var payload2 = web3.fromAscii(this.state.lastName,32);
+    var payload3 = web3.toBigNumber(this.state.age);
+    var payload = ("'" + payload1 + "','" + payload2 + "',"+ payload3);
+    console.log(payload);
+    console.log(web3.eth.defaultAccount);
+    People.addPerson(payload1, payload2, payload3, {from: web3.eth.accounts[0], gas: 3000000})
+    // .then(function(result){
+    //   console.log(result);
+    // });
+    this.setState({
+      firstName: '',
+      lastName: '',
+      age: '',
+    });
+    // this.props.updateTransactionTime();
+  }
+
+  render() {
+    const formStyle = {
+      "backgroundColor": "deepskyblue",
+      "flexGrow": 1,
+    };
+    return (
+      <div className="form" style={formStyle}>
+        <form>
+          <label>
+            First Name:
+            <input id="firstName" onChange={this.handleTextChange} type="text" value={this.state.firstName}/>
+          </label>
+          <hr/>
+          <label>
+            Last Name:
+            <input id="lastName" onChange={this.handleTextChange} type="text" value={this.state.lastName}/>
+          </label>
+          <hr />
+          <label>
+            Age:
+            <input id="age" onChange={this.handleTextChange} type="text" value={this.state.age}/>
+          </label>
+          <hr />
+          <input id="submit" type="button" value="Submit" onClick={this.handleSubmit}/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
